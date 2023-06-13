9788d903a2166d8ab1bd3247fed07bea37b7f457
Rob Bieman <rob.bieman@student.uva.nl>
2023-06-13 11:55:52 +0200

    camera input added lab2

--- Lab_week2/test.py
+++ Lab_week2/test.py
@@ -15,7 +15,7 @@ def get_pixel(img, coord):
         return np.array((0, 0, 0))
     return img[y][x]
 
-cap = cv2.VideoCapture("files/Big.Buck.Bunny.mp4")
+cap = cv2.VideoCapture(0)
 
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -25,40 +25,35 @@ angle = math.pi / 7
 
 rot_matrix = get_rot_matrix(-angle)
 
-sample_rate = 10
-frame_nr = 0
-while True:
-    succes, img = cap.read()
-    if not succes:
-        break
-    if frame_nr % sample_rate:
-        continue
+succes, img = cap.read()
+if not succes:
+    exit()
 
-    for y, column in enumerate(output):
-        for x, _ in enumerate(column):
-            old_coords = np.matmul(rot_matrix, np.array([[x - width / 2], [y - height / 2]]))
-            old_coords += np.array([[width / 2], [height / 2]])
-            old_coords.resize(2,)
-            old_x, old_y = old_coords
+for y, column in enumerate(output):
+    for x, _ in enumerate(column):
+        old_coords = np.matmul(rot_matrix, np.array([[x - width / 2], [y - height / 2]]))
+        old_coords += np.array([[width / 2], [height / 2]])
+        old_coords.resize(2,)
+        old_x, old_y = old_coords
 
-            x1 = math.floor(old_x)
-            x2 = x1 + 1
-            y1 = math.floor(old_y)
-            y2 = y1 + 1
+        x1 = math.floor(old_x)
+        x2 = x1 + 1
+        y1 = math.floor(old_y)
+        y2 = y1 + 1
 
-            w11 = (x2 - old_x) * (y2 - old_y)
-            w12 = (x2 - old_x) * (old_y - y1)
-            w21 = (old_x - x1) * (y2 - old_y)
-            w22 = (old_x - x1) * (old_y - y1)
+        w11 = (x2 - old_x) * (y2 - old_y)
+        w12 = (x2 - old_x) * (old_y - y1)
+        w21 = (old_x - x1) * (y2 - old_y)
+        w22 = (old_x - x1) * (old_y - y1)
 
-            pixel = (
-                w11 * get_pixel(img, (x1, y1))
-                + w12 * get_pixel(img, (x1, y2))
-                + w21 * get_pixel(img, (x2, y1))
-                + w22 * get_pixel(img, (x2, y2))
-            )
+        pixel = (
+            w11 * get_pixel(img, (x1, y1))
+            + w12 * get_pixel(img, (x1, y2))
+            + w21 * get_pixel(img, (x2, y1))
+            + w22 * get_pixel(img, (x2, y2))
+        )
 
-            output[y][x] = pixel / 255
+        output[y][x] = pixel / 255
 
-    plt.imshow(output)
-    plt.show()
+plt.imshow(output)
+plt.show()
